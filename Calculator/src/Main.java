a2b75196319e423da0daf2b5c985a900558d2635
Andrey Sergeev <148659604+AndreeeeeySergeev@users.noreply.github.com>
2024-11-23 19:42:42 +0300

    Add files via upload

---
+++ Calculator/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int firstNumber;
+        int changing;
+        String secondNumber;
+        int thirdNumber;
+        int finalNumber;
+
+        Scanner input = new Scanner(System.in);
+
+            System.out.print("Введите первое число: ");
+            firstNumber = input.nextInt();
+
+            System.out.print("Введите знак операции (+;-;*;/): ");
+            secondNumber = input.next();
+            changing = secondNumber.charAt(0);
+
+
+            System.out.print("Введите второе число: ");
+            thirdNumber = input.nextInt();
+
+            switch (changing) {
+                case '+':
+                    finalNumber = firstNumber + thirdNumber;
+                    System.out.println("Итого: " + finalNumber);
+                    break;
+                case '-':
+                    finalNumber = firstNumber - thirdNumber;
+                    System.out.println("Итого: " + finalNumber);
+                    break;
+                case '*':
+                    finalNumber = firstNumber * thirdNumber;
+                    System.out.println("Итого: " + finalNumber);
+                    break;
+                case '/':
+                    finalNumber = firstNumber / thirdNumber;
+                    System.out.println("Итого: " + finalNumber);
+                    break;
+                default:
+                    System.out.println("Вы ввели неверный знак");
+            }
+        }
+    }
+
